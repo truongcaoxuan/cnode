b9b688f903e350bd23cbe55a6e402b3caa15b5cf
Truong Cao Xuan <54384725+truongcaoxuan@users.noreply.github.com>
2022-05-18 09:13:00 +0700

    Update poolstatsUpdater.sh

--- scripts/poolstatsUpdater.sh
+++ scripts/poolstatsUpdater.sh
@@ -4,4 +4,4 @@ curl https://js.adapools.org/pools/f25197cb96c05ec03b8fc5ef195ea87e76ba8fab3b0dc
 | jq '.data | del(.hist_bpe, .handles, .hist_roa, .pool_id_bech32, .db_ticker, .db_name, .db_url, .ticker_orig, .pool_id,  .group_basic, .tax_ratio_old, .tax_fix_old , .direct, .db_description)' \
 | tr -d \"{},: \
 | awk NF \
-| sed -e 's/^[ \t]*/tradapool_/' > /opt/cardano/cnode-docker-compose/exporters/poolstats/tradapools.prom
+| sed -e 's/^[ \t]*/tradapool_/' > /opt/cardano/cnode-docker-compose/exporters/poolstats/tradapool.prom
